728781957aefbc231a6c9deed9ef05307a8db2b4
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-06-27 22:31:41 -0700

    Removed unused code

--- db/fakeData.js
+++ db/fakeData.js
@@ -23,37 +23,6 @@ for (let i = 0; i < 100; i += 1) {
 }
 
 review.insertMany(fakeData)
-
-  // review.insertMany([
-  //   {    
-  //     id: 1,
-  //     image_url: 'https://www.heritagetours.com/wp-content/uploads/2017/12/Seondary-Page-Hero-Spain-1.jpg',
-  //     reviewer_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  //     star_rate: Math.floor(Math.random() * 6),
-  //     likes_count: Math.floor(Math.random() * 1000),
-  //   },
-  //   {    
-  //     id: 2,
-  //     image_url: 'https://cdn.thecrazytourist.com/wp-content/uploads/2017/11/Kaiserburg.jpg',
-  //     reviewer_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  //     star_rate: Math.floor(Math.random() * 6),
-  //     likes_count: Math.floor(Math.random() * 1000),
-  //   },
-  //   {    
-  //     id: 3,
-  //     image_url: 'https://www.nyhabitat.com/blog/wp-content/uploads/2014/04/Paris-france-summer-guide-eiffel-tower-trocadero-fountains.jpg',
-  //     reviewer_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  //     star_rate: Math.floor(Math.random() * 6),
-  //     likes_count: Math.floor(Math.random() * 1000),
-  //   },
-  //   {    
-  //     id: 4,
-  //     image_url: 'https://i.ytimg.com/vi/4W2c7JJdZQY/maxresdefault.jpg',
-  //     reviewer_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  //     star_rate: Math.floor(Math.random() * 6),
-  //     likes_count: Math.floor(Math.random() * 1000),
-  //   },
-  // ])
   .then(() => {
     console.log('data is successfully seeded!');
   });
